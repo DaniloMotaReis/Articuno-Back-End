ee122d7f8dda7dad92ed88f694447ac037ae701f
Danilo Mota Reis <101838987+DaniloMotaReis@users.noreply.github.com>
2022-12-17 00:20:21 -0300

    initial commit

---
+++ src/main/java/br/com/articuno/ArticunoApplication.java
@@ -0,0 +1,13 @@
+package br.com.articuno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArticunoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArticunoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/articuno/ArticunoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.articuno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArticunoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
